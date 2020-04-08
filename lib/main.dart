13742b41fe880b6027396aaae804cb0e4ef7560c
Vicente Santiago <vicentesantiago1603@gmail.com>
2020-04-07 22:01:38 -0300

    correção na autenticação do chat

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:micro_news/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'models/user_model.dart';
-import 'screens/home_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
     return ScopedModel<UserModel>(
       model: UserModel(),
         child: MaterialApp(
-            title: "Teste MicroNews",
+            title: "MicroNews",
             theme: ThemeData(
                 primarySwatch: Colors.blue,
                 primaryColor: Colors.blueAccent),
