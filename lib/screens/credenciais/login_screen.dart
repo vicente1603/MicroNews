8ee896c69992cbaf3e72a6f9237e0a0beb04cdec
Vicente Jose Santiago Costa Oliveira <vicente.oliveira@MJV-28475.home>
2021-05-10 18:34:20 -0300

    inclusao do login por biometria nos dispositivos iOS

--- lib/screens/credenciais/login_screen.dart
+++ lib/screens/credenciais/login_screen.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
-import 'package:micro_news/blocs/app_bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:micro_news/models/usuario_model.dart';
 import 'package:micro_news/screens/credenciais/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:micro_news/tabs/home_tab.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:provider/provider.dart';
 import 'package:scoped_model/scoped_model.dart';
-import '../home_screen.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -21,15 +21,27 @@ class _LoginScreenState extends State<LoginScreen> {
 
     readcontentEmail();
     readcontentPass();
+
+    readcontentEmailBiometric();
+    readcontentPassBiometric();
+
+    getLeitorDigital();
+    getAutorizacaoDigital();
   }
 
   final _emailController = TextEditingController();
   final _senhaController = TextEditingController();
 
+  String emailPrefs;
+  String senhaPrefs;
+  bool leitorDigital = false;
+  bool biometriaAutorizada = false;
+  bool loginAutorizado = false;
+  bool remember = false;
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  bool remember = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +50,8 @@ class _LoginScreenState extends State<LoginScreen> {
         appBar: AppBar(
           elevation: 0.0,
           backgroundColor: Colors.blueAccent,
-          title: Text("MicroNews®", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
+          title: Text("MicroNews®",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
           centerTitle: true,
           actions: <Widget>[
             FlatButton(
@@ -108,6 +121,49 @@ class _LoginScreenState extends State<LoginScreen> {
                       Text("Lembrar-me"),
                     ],
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Leitor biométrico", style: TextStyle(fontSize: 17)),
+                      SizedBox(width: 15),
+                      AnimatedContainer(
+                        duration: Duration(milliseconds: 0),
+                        height: 40.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: leitorDigital
+                              ? Colors.blueAccent.withOpacity(0.5)
+                              : Colors.redAccent.withOpacity(0.5),
+                        ),
+                        child: Stack(
+                          children: <Widget>[
+                            AnimatedPositioned(
+                              duration: Duration(milliseconds: 0),
+                              curve: Curves.easeIn,
+                              top: 3.0,
+                              left: leitorDigital ? 25.0 : 00,
+                              right: leitorDigital ? 0.0 : 25.0,
+                              child: InkWell(
+                                onTap: _leitorDigital,
+                                child: AnimatedSwitcher(
+                                    duration: Duration(milliseconds: 0),
+                                    child: leitorDigital
+                                        ? Icon(Icons.check_circle,
+                                            color: Colors.lightBlue[900],
+                                            size: 35.0,
+                                            key: UniqueKey())
+                                        : Icon(Icons.remove_circle_outline,
+                                            color: Colors.red,
+                                            size: 35.0,
+                                            key: UniqueKey())),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                   Align(
                     alignment: Alignment.centerRight,
                     child: FlatButton(
@@ -162,10 +218,21 @@ class _LoginScreenState extends State<LoginScreen> {
     if (remember == true) {
       writeContentPass();
       writeContentEmail();
-    }else {
+    } else {
       cleanContentEmail();
       cleanContentSenha();
     }
+
+    if (leitorDigital) {
+      if (biometriaAutorizada == false) {
+        biometriaAutorizada = true;
+        setAutorizacaoDigital(biometriaAutorizada);
+      }
+    } else {
+      biometriaAutorizada = false;
+    }
+
+    loginAutorizado = true;
 
     Navigator.of(context)
         .pushReplacement(MaterialPageRoute(builder: (context) => HomeTab()));
@@ -192,6 +259,21 @@ class _LoginScreenState extends State<LoginScreen> {
   Future<File> get _localFilePass async {
     final path = await _localPath;
     return File("$path/dataP.txt");
+  }
+
+  Future<String> get _localPathBiometric async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFileEmailBiometric async {
+    final path = await _localPathBiometric;
+    return File("$path/dataEBiometric.txt");
+  }
+
+  Future<File> get _localFilePassBiometric async {
+    final path = await _localPathBiometric;
+    return File("$path/dataPBiometric.txt");
   }
 
   Future<File> writeContentEmail() async {
@@ -259,5 +341,144 @@ class _LoginScreenState extends State<LoginScreen> {
     } catch (e) {
       return 'Error';
     }
+  }
+
+  Future<String> readcontentEmailBiometric() async {
+    try {
+      final file = await _localFileEmailBiometric;
+      String contents = await file.readAsString();
+      emailPrefs = contents;
+      return contents;
+    } catch (e) {
+      return 'Error';
+    }
+  }
+
+  Future<String> readcontentPassBiometric() async {
+    try {
+      final file = await _localFilePassBiometric;
+      String contents = await file.readAsString();
+      senhaPrefs = contents;
+      return contents;
+    } catch (e) {
+      return 'Error';
+    }
+  }
+
+  getLeitorDigital() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    leitorDigital = prefs.getBool('leitorDigital');
+
+    if (leitorDigital == null) {
+      leitorDigital = false;
+    }
+
+    setState(() {});
+  }
+
+  getAutorizacaoDigital() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    biometriaAutorizada = prefs.getBool('biometriaAutorizada');
+    if (biometriaAutorizada == null) {
+      biometriaAutorizada = false;
+    }
+
+    if (loginAutorizado == false) {
+      abrirTelaDigital();
+    }
+  }
+
+  abrirTelaDigital() {
+    if (biometriaAutorizada) {
+      _authorizeNow();
+    }
+  }
+
+  void _authorizeNow() async {
+    bool isAuthorized = false;
+
+    try {
+      isAuthorized = await _localAuthentication.authenticateWithBiometrics(
+          localizedReason: "Toque no sensor para realizar o login",
+          useErrorDialogs: true,
+          stickyAuth: false);
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      if (isAuthorized) {
+        UserModel user = UserModel();
+        user.signIn(
+            email: _emailController.text.trim(),
+            pass: _senhaController.text.trim(),
+            onSuccess: _onSuccess,
+            onFail: _onFail);
+      } else {
+        print("falha no login");
+      }
+    });
+  }
+
+  _leitorDigital() {
+    setState(() {
+      leitorDigital = !leitorDigital;
+      if (leitorDigital) {
+        _mostrarDialogLeitorDigital();
+      } else {
+        setAutorizacaoDigital(false);
+      }
+      setLeitorDigital(leitorDigital);
+    });
+  }
+
+  setAutorizacaoDigital(bool biometriaAutorizada) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('biometriaAutorizada', biometriaAutorizada);
+
+    writeContentEmailBiometric();
+    writeContentPassBiometric();
+
+    getAutorizacaoDigital();
+
+    setState(() {});
+  }
+
+  setLeitorDigital(bool leitorDigital) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('leitorDigital', leitorDigital);
+  }
+
+  _mostrarDialogLeitorDigital() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Leitor biométrico"),
+            content:
+                Text("O leitor biométrico será habilitado no próximo login."),
+            actions: <Widget>[
+              FlatButton(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop(
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  })
+            ],
+          );
+        });
+  }
+
+  Future<File> writeContentEmailBiometric() async {
+    final file = await _localFileEmailBiometric;
+    return file.writeAsString(_emailController.text.trim());
+  }
+
+  Future<File> writeContentPassBiometric() async {
+    final file = await _localFilePassBiometric;
+    return file.writeAsString(_senhaController.text.trim());
   }
 }
